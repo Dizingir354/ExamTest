6434ad016aa38ac606e4e8348c699afd59fedb88
Dizingir354 <Dizingir355@gmail.com>
2022-08-11 01:38:00 +0300

    Two day work
    I working 2 hours in Two day

--- ExamTwo/ExamTwo/Test.h
+++ ExamTwo/ExamTwo/Test.h
@@ -15,6 +15,11 @@ public:
 		surName = surNameUser;
 		name = nameUser;
 	}
+	void deleteName()
+	{
+		surName = nullptr;
+		name = nullptr;
+	}
 };
 
 enum typeOfSchoolSubject
@@ -22,45 +27,121 @@ enum typeOfSchoolSubject
 	DiscreteMath, mathAnalysis, quantumPhysics, mechanics
 };
 
-class Admin
+class Admin : public FullName
 {
-
-	void doQuize(typeOfSchoolSubject subject);
-public:
-	void createQuize(typeOfSchoolSubject subject);
 	
+
+	Student* allYourStudent;
+	int sizeStudent;
+public:
+	void createStudent(string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser);
+	void deleteStudent(int pos);
+	void newCreateStudent(int pos, string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser);
+	void printResultTest();
+	void createQuize(typeOfSchoolSubject subject);
+	void doTest(typeOfSchoolSubject subject);
 };
 
+void Admin::createStudent(string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser)
+{
+	allYourStudent[sizeStudent]->name.createName(surNameUser, nameUser);
+	allYourStudent[sizeStudent]->password = passwordUser;
+	allYourStudent[sizeStudent]->address = addressUser;
+	allYourStudent[sizeStudent]->numberPhone = numberPhoneUser;
+}
 
+void Admin::deleteStudent(int pos)
+{
+	if (pos == -1)
+	{
+		allYourStudent[sizeStudent].deleteName();
+		allYourStudent[sizeStudent]->password = nullptr;
+		allYourStudent[sizeStudent]->address = nullptr;
+		allYourStudent[sizeStudent]->numberPhone = nullptr;
+	}
+	else
+	{
+		allYourStudent[pos].deleteName();
+		allYourStudent[pos]->password = nullptr;
+		allYourStudent[pos]->address = nullptr;
+		allYourStudent[pos]->numberPhone = nullptr;
+	}
+}
+
+void Admin::newCreateStudent(int pos, string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser)
+{
+	deleteStudent(pos);
+	createStudent(surNameUser, nameUser, passwordUser, addressUser, numberPhoneUser);
+}
+
+void Admin::printResultTest()
+{
+	cout << "Print test what you want see:\n1)DiscreteMath;\n2)mathAnalysis;\n3)quantumPhysics;\n4)mechanics;\n5)nothing: ";
+	int vibor;	cin >> vibor;	cout << endl;
+	typeOfSchoolSubject subject;
+	switch (vibor)
+	{
+	case 1:
+		subject = DiscreteMath;
+		break;
+	case 2:
+		subject = mathAnalysis;
+		break;
+	case 3:
+		subject = quantumPhysics;
+		break;
+	case 4:
+		subject = mechanics;
+		break;
+	case 5:
+		break;
+	default:
+		break;
+	}
+	cout << "Print name student what you want see:";
+	string name;	cin >> name;	cout << endl;
+}
 
 class Student : public Admin
 {
+private:
 	FullName name;
 	string password;
 	int address;
 	int numberPhone;
 
 public:
-	Student() {}
+	Student() { cout << "Constr!" << endl; }
 	~Student();
 	void registration(string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser);
+	void deleteReg();
 	void tests();
 	void math();
 	void physics();
-	void mechanics();
+	void Mechanics();
 };
 
 Student::~Student()
 {
-	cout << "End!" << endl;
+	cout << "Destr!" << endl;
 }
 
 void Student::registration(string surNameUser, string nameUser, string passwordUser, int addressUser, int numberPhoneUser)
 {
+	createStudent(surNameUser, nameUser, passwordUser, addressUser, numberPhoneUser);
 	name.createName(surNameUser, nameUser);
 	password = passwordUser;
 	address = addressUser;
 	numberPhone = numberPhoneUser;
+}
+
+void Student::deleteReg()
+{
+	deleteStudent(-1);
+	deleteName();
+	password = nullptr;
+	address = nullptr;
+	numberPhone = nullptr;
 }
 
 void Student::tests()
@@ -71,10 +152,13 @@ void Student::tests()
 	{
 	case 1:
 		math();
+		break;
 	case 2:
 		physics();
+		break;
 	case 3:
-		mechanics();
+		Mechanics();
+		break;
 	default:
 		break;
 	}
@@ -82,14 +166,48 @@ void Student::tests()
 
 void Student::math()
 {
-	cout << "You reale want do in test?\n1)DiscreteMath;\n2)mathAnalysis: ";
+	cout << "What you want do in test?\n1)DiscreteMath;\n2)mathAnalysis: ";
 	int vibor; cin >> vibor;
 	switch (vibor)
 	{
 	case 1:
-		
+		doTest(DiscreteMath);
+		break;
 	case 2:
-		
+		doTest(mathAnalysis);
+		break;
+	default:
+		break;
+	}
+}
+
+void Student::physics()
+{
+	cout << "Do you want do this test?\n1)Yes(do quantumPhysics);\n2)No: ";
+	int vibor; cin >> vibor;
+	switch (vibor)
+	{
+	case 1:
+		doTest(quantumPhysics);
+		break;
+	case 2:
+		break;
+	default:
+		break;
+	}
+}
+
+void Student::Mechanics()
+{
+	cout << "Do you want do this test?\n1)Yes(do mechanics);\n2)No: ";
+	int vibor; cin >> vibor;
+	switch (vibor)
+	{
+	case 1:
+		doTest(mechanics);
+		break;
+	case 2:
+		break;
 	default:
 		break;
 	}
